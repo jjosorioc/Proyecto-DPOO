88b94101544c7be890973cfaa5ec0d357a49f0af
Juan José Osorio <j.osorioc@uniandes.edu.co>
2021-11-08 22:03:54 -0500

    Paquete del encargado del inventario

---
+++ src/encargadoInventario/VentanaPrincipal.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package encargadoInventario;
+
+
+public class VentanaPrincipal
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
