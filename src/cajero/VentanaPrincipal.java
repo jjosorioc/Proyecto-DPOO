641f5b4d3e3de95bc7bbd5ac177ce90c843a46b6
Juan José Osorio <j.osorioc@uniandes.edu.co>
2021-11-08 22:05:52 -0500

    Paquete para el cajero

---
+++ src/cajero/VentanaPrincipal.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package cajero;
+
+
+public class VentanaPrincipal
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
